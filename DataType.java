b593dad8debadb4def3a4f657fc8938f09e0b516
ranik06 <115772391+ranik06@users.noreply.github.com>
2022-10-14 11:12:09 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,24 @@
+/* program: to understand various data type in java
+author:     rani
+@date:      14 oct 2022
+
+*/
+class DataType
+{
+	//main methos
+    public static void main(String args[])
+    {
+		//declaring local variable
+		int number1=52;
+		int number2=63;
+		//calculating sum
+		int sum = number1+number2;
+		System.out.println("sum of the no. is ="+sum);
+	
+	
+	
+	
+    }
+//end of main
+
+}//end of class datatype
